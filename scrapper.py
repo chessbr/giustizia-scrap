7801c42111bf2f7b7964be93446885f08e9e0a6a
chessbr <christianhess.rlz@gmail.com>
2018-11-22 21:34:24 -0200

    Add doc

--- scrapper.py
+++ scrapper.py
@@ -1,7 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Scrapper para buscar informações de processos da Giustizia Civile
+
+Você vai precisar de um UUID válido e um Token.
+Para conseguir os mesmo, você deve instalar o aplicativo em um device real
+e capturar estas informações através de um MITM, eu usei o https://mitmproxy.org/ pois é simples e free.
+
+Atenção! Não faça muitas requisições em pouco tempo, isso vai bloquear o seu UUID/token e IP.
+Além disso, não seria bom perder este acesso por uso maldoso. Faça um bom uso.
+"""
 import requests
 import time
 from datetime import datetime
-import os
 
 INITIAL_ID = 45000
 FINAL_ID = 52000
@@ -25,7 +35,7 @@ payload = dict(
 for process_id in range(INITIAL_ID, FINAL_ID, 1):
     payload.update(dict(
         numproc=process_id,
-        _=int(datetime.now().timestamp())
+        _=int(datetime.now().timestamp())       # este parâmetro é o tipespam, tem que ser diferente para cada request
     ))
     response = requests.get("https://mob1.processotelematico.giustizia.it/proxy/index_mobile.php", params=payload)
     content = response.text
