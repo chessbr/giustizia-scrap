b4b215b983314119800d2564f213aae53927cf3c
Nick Lee <email@nickle.es>
2018-12-05 09:17:41 +0100

    Record judgement numbers from case

--- lib/giustizia.py
+++ lib/giustizia.py
@@ -10,8 +10,8 @@ from secrets import (
     user_agent, uuid
 )
 
-query_url = "https://mob1.processotelematico.giustizia.it/proxy/index_mobile.php"
-base_payload = dict(
+QUERY_URL = "https://mob1.processotelematico.giustizia.it/proxy/index_mobile.php"
+BASE_PAYLOAD = dict(
     version="1.1.13",
     platform=os_version,
     uuid=uuid,
@@ -24,17 +24,17 @@ base_payload = dict(
     idufficio="0580910098",
     tipoufficio=1
 )
-headers = {
+HEADERS = {
     'User-Agent': user_agent
 }
 
-inscrito_ruolo_re = re.compile("\<li\>iscritto al ruolo il (.+)\<\/li\>")
-remove_lawyer_prefix = re.compile("(?<=Avv. ).*")
+RE_INSCRITO_RUOLO = re.compile("\<li\>iscritto al ruolo il (.+)\<\/li\>")
+RE_REMOVE_LAWYER_PREFIX = re.compile("(?<=Avv. ).*")
 
 
 class Case:
     def __init__(self, case_yr, case_no, date_filed, judge_name, date_hearing, case_state, primary_lawyer_initials,
-                 raw_case_content=None):
+                 raw_case_content=None, judgement_number=None):
         self.year = case_yr
         self.number = case_no
         self.date_filed = date_filed
@@ -43,6 +43,7 @@ class Case:
         self.case_state = case_state
         self.primary_lawyer_initials = primary_lawyer_initials
         self.raw_case_content = raw_case_content
+        self.judgement_number = judgement_number
 
     def __str__(self):
         return ";".join([
@@ -52,6 +53,7 @@ class Case:
             self.date_hearing,
             self.case_state,
             self.primary_lawyer_initials,
+            self.judgement_number,
         ])
 
     def asdict(self):
@@ -63,12 +65,13 @@ class Case:
             'judge_name': self.judge_name,
             'case_state': self.case_state,
             'primary_lawyer_initials': self.primary_lawyer_initials,
-            'raw_case_content': self.raw_case_content
+            'judgement_number': self.judgement_number,
+            'raw_case_content': self.raw_case_content,
         }
 
 
 def get_case_details(case_yr, case_no):
-    payload = base_payload.copy()
+    payload = BASE_PAYLOAD.copy()
 
     payload.update(dict(
         aaproc=str(case_yr),
@@ -76,7 +79,7 @@ def get_case_details(case_yr, case_no):
         _=int(datetime.now().timestamp())
     ))
 
-    response = requests.get(query_url, params=payload, headers=headers)
+    response = requests.get(QUERY_URL, params=payload, headers=HEADERS)
     content = response.text
 
     if "Errore tecnico" in content:
@@ -88,7 +91,7 @@ def get_case_details(case_yr, case_no):
         nome_giudice = bs.find("nomegiudice")
         data_udienza = bs.find("dataudienza")
 
-        inscrito_ruolo_search = inscrito_ruolo_re.search(content)
+        inscrito_ruolo_search = RE_INSCRITO_RUOLO.search(content)
         if inscrito_ruolo_search:
             data_inscricao = inscrito_ruolo_search.groups()[0]
         else:
@@ -101,6 +104,8 @@ def get_case_details(case_yr, case_no):
 
         primary_lawyer_initials = extract_primary_lawyer_initials(bs) or "Unknown"
 
+        judgement_number = extract_judgement_number(bs)
+
         return Case(
             case_yr,
             case_no,
@@ -109,7 +114,8 @@ def get_case_details(case_yr, case_no):
             data_udienza,
             case_state,
             primary_lawyer_initials,
-            raw_case_content=content
+            raw_case_content=content,
+            judgement_number=judgement_number
         )
 
 
@@ -129,8 +135,19 @@ def extract_primary_lawyer_initials(bs_content):
         case_state_list_copy = bs_content.findAll("li")
         for idx, val in enumerate(case_state_list_copy):
             if val.contents[0] == 'Parti fascicolo':
-                redacted_name = remove_lawyer_prefix.search(case_state_list_copy[idx + 1].contents[3]).group(0)
+                redacted_name = RE_REMOVE_LAWYER_PREFIX.search(case_state_list_copy[idx + 1].contents[3]).group(0)
                 return redacted_name.replace(' ', '').replace('*', '')
+        return None
+    except:
+        return None
+
+
+def extract_judgement_number(bs_content):
+    try:
+        li_entries = bs_content.findAll("li")
+        for idx, val in enumerate(li_entries):
+            if val.contents[0] == 'Sentenza definitiva':
+                return li_entries[idx + 1].contents[0][4:]
         return None
     except:
         return None
